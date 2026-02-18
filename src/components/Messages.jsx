ef84773dd2c880d9ed5e7372d1b182fc825c7d22
Aryan Singh <97840303+OaryanO@users.noreply.github.com>
2026-02-18 13:00:07 +0530

    Refactor Messages component to handle empty state

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,3 +1,30 @@
+// import React from 'react'
+// import Message from './Message'
+// import useGetMessages from '../hooks/useGetMessages';
+// import { useSelector } from "react-redux";
+// import useGetRealTimeMessage from '../hooks/useGetRealTimeMessage';
+
+// const Messages = () => {
+//     useGetMessages();
+//     useGetRealTimeMessage();
+//     const { messages } = useSelector(store => store.message);
+//     return (
+//         <div className='px-4 flex-1 overflow-auto'>
+//             {
+//                messages && messages?.map((message) => {
+//                     return (
+//                         <Message key={message._id} message={message} />
+//                     )
+//                 })
+//             }
+
+//         </div>
+
+
+//     )
+// }
+
+
 import React from 'react'
 import Message from './Message'
 import useGetMessages from '../hooks/useGetMessages';
@@ -7,21 +34,24 @@ import useGetRealTimeMessage from '../hooks/useGetRealTimeMessage';
 const Messages = () => {
     useGetMessages();
     useGetRealTimeMessage();
+
     const { messages } = useSelector(store => store.message);
+
     return (
         <div className='px-4 flex-1 overflow-auto'>
-            {
-               messages && messages?.map((message) => {
-                    return (
-                        <Message key={message._id} message={message} />
-                    )
-                })
-            }
-
+            {messages?.length > 0 ? (
+                messages.map((msg) =>
+                    msg ? <Message key={msg._id} message={msg} /> : null
+                )
+            ) : (
+                <p className="text-center text-gray-400 mt-4">
+                    No messages yet
+                </p>
+            )}
         </div>
-
-
     )
 }
 
-export default Messages
+export default Messages;
+
+// export default Messages
