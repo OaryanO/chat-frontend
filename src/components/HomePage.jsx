42ea655f9322c1d8908b96ea078714294eaa6f5d
Aryan Singh <97840303+OaryanO@users.noreply.github.com>
2026-02-18 11:28:53 +0530

    check

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import Sidebar from './Sidebar'
 import MessageContainer from './MessageContainer'
 import { useSelector } from 'react-redux'
@@ -7,17 +7,19 @@ import { useNavigate } from 'react-router-dom'
 const HomePage = () => {
   const { authUser } = useSelector(store => store.user);
   const navigate = useNavigate();
+
   useEffect(() => {
     if (!authUser) {
       navigate("/login");
     }
-  }, []);
+  }, [authUser, navigate]);
+
   return (
     <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
       <Sidebar />
       <MessageContainer />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
